52d091c82d447a2558ff1dc342f7f083b961978d
Bob Strecansky <bob.strecansky@gmail.com>
2020-04-21 08:03:03 -0400

    CHANGEME Passwords, Updating Alerting Rules

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-hey -n 1000 -c 5 http://goapp:2112/fast_response
-hey -n 100 -c 1 http://goapp:2112/error_response
+hey -n 100 -c 5 http://goapp:2112/fast_response
+hey -n 12 -c 1 http://goapp:2112/error_response
+
