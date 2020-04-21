bdf340979c361d9047d424aff20b45d44b4c8564
Bob Strecansky <bob.strecansky@gmail.com>
2020-04-20 22:53:09 -0400

    Correct RED metrics, removing gin

--- goapp/cmd/main.go
+++ goapp/cmd/main.go
@@ -1,43 +1,70 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"math/rand"
+	"net/http"
 	"time"
 
-	"github.com/gin-gonic/gin"
-	ginprometheus "github.com/zsais/go-gin-prometheus"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	r := gin.New()
-	// Optional custom metrics list
-	customMetrics := []*ginprometheus.Metric{
-		&ginprometheus.Metric{
-			ID:          "1234",          // optional string
-			Name:        "histogram_vec", // required string
-			Description: "histogram_vec", // required string
-			Type:        "histogram_vec", // required string
+	requests := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "requests",
+			Help: "A counter for the requests signal",
 		},
+		[]string{"code", "method"},
+	)
+
+	duration := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "duration",
+			Help:    "A histogram of latencies for the duration signal",
+			Buckets: []float64{.025, .05, 0.1, 0.25, 0.5, 0.75},
+		},
+		[]string{"handler", "method"},
+	)
+
+	timedSignalHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		sleepTime := rand.Intn(1000)
+		time.Sleep(time.Duration(sleepTime) * time.Millisecond)
+		elapsed := time.Since(start)
+		fmt.Fprintln(w, "Request Completed - duration %s", elapsed)
+		log.Printf("Request Completed - duration %s", elapsed)
+	})
+
+	errorSignalHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		sleepTime := rand.Intn(1000)
+		time.Sleep(time.Duration(sleepTime) * time.Millisecond)
+		elapsed := time.Since(start)
+		http.Error(w, "Server Error", http.StatusInternalServerError)
+		log.Printf("Request Errored - duration %s", elapsed)
+	})
+
+	timedSignalChain := promhttp.InstrumentHandlerDuration(duration.MustCurryWith(prometheus.Labels{"handler": "signals"}),
+		promhttp.InstrumentHandlerCounter(requests, timedSignalHandler),
+	)
+
+	errorSignalChain := promhttp.InstrumentHandlerDuration(duration.MustCurryWith(prometheus.Labels{"handler": "signals"}),
+		promhttp.InstrumentHandlerCounter(requests, errorSignalHandler),
+	)
+
+	prometheus.MustRegister(requests, duration)
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/fast_response", timedSignalChain)
+	http.Handle("/error_response", errorSignalChain)
+	s := &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":2112",
 	}
-	p := ginprometheus.NewPrometheus("go_sli_slo_app", customMetrics)
-
-	p.Use(r)
-
-	r.GET("/fast_response", func(c *gin.Context) {
-		rand.Seed(time.Now().UnixNano())
-		n := rand.Intn(1000)
-		time.Sleep(time.Duration(time.Duration(n) * time.Millisecond))
-		c.JSON(200, gin.H{"Response Time (ms)": n})
-	})
-
-	r.GET("/slow_response", func(c *gin.Context) {
-		time.Sleep(10 * time.Second)
-		c.JSON(200, "Slow Response")
-	})
-
-	r.GET("/error_response", func(c *gin.Context) {
-		c.JSON(500, "Error Response")
-	})
-
-	r.Run(":2112")
+	log.Printf("4 Golden Signals Server Listening")
+	s.ListenAndServe()
 }
